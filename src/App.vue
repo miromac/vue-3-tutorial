ee4976c7ba4010d3517628ce4d4d8562329eb492
Miro Mac <itcodevelopment@gmail.com>
2022-03-08 18:40:56 +0200

    Interpolation, methods & event listeners

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div>
+      <button @click="addLike">Like</button>
+      <button @click="addDislike">Dislike</button>
+    </div>
+    <div>Amount of likes: <strong>{{ likes }}</strong></div>
+    <div>Amount of dislikes: <strong>{{ dislikes }}</strong></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      likes: 0,
+      dislikes: 5,
+    }
+  },
+  methods: {
+    addLike() {
+      this.likes += 1;
+    },
+    addDislike() {
+      this.dislikes += 1;
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
+
+// Single file component
