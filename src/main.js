f71b9256ef7a23cfec91c210b713bb11372c71d1
Miro Mac <itcodevelopment@gmail.com>
2022-03-12 11:38:41 +0200

    V-Focus directive

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import components from '@/components/UI'
-import router from './router/router'
+import router from '@/router/router'
+import VFocus from '@/directives/VFocus'
 
 const app = createApp(App)
 
 components.forEach(component => {
   app.component(component.name, component)
 })
+
+app.directive('focus', VFocus)
 
 app
     .use(router)
--- src/pages/PostPage.vue
+++ src/pages/PostPage.vue
@@ -4,6 +4,7 @@
     <my-input
       v-model="searchQuery"
       placeholder="Search..."
+      v-focus
     />
     <div class="app_btns">
       <my-button
